cd0945116f1fb30e5111d84368c0649f7279c5ff
ripon4521 <riponakondo4521@gmail.com>
2024-03-14 23:55:33 +0600

    implement the chekbox

--- src/components/CheckButton.jsx
+++ src/components/CheckButton.jsx
@@ -24,28 +24,13 @@ const CheckButton = ({ checked, handleCheck }) => {
   return (
     <motion.div
       animate={checked ? 'checked' : 'unchecked'}
-      className="w-8 h-8 cursor-pointer"
+     
+      className="w-8 h-8 cursor-pointer  "
       variants={boxVariants}
-      onClick={handleCheck}
     >
-      <motion.svg
-        className="w-full h-full"
-        viewBox="0 0 53 38"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <motion.path
-          variants={checkVariants}
-          animate={checked ? 'checked' : 'unchecked'}
-          style={{ pathLength, opacity }}
-          fill="none"
-          strokeMiterlimit="10"
-          strokeWidth="6"
-          d="M1.5 22L16 36.5L51.5 1"
-          strokeLinejoin="round"
-          strokeLinecap="round"
-        />
-      </motion.svg>
+ 
+    <input  onClick={handleCheck} type="checkbox" defaultChecked className="checkbox checkbox-secondary" />
+
     </motion.div>
   );
 };
